8f111c5ad6b033e1ed4591caadcfa1c713faf2ab
alexandr-khvatov <alexkhvatov.dev@gmail.com>
2023-05-02 00:59:00 +0400

    add: add all :)

--- etc-ssl/gen-cert.sh
+++ etc-ssl/gen-cert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 IP=212.109.219.35
-KEY_OUT="certs/$IP/webhook_pkey.pem"
-CERT_OUT="certs/$IP/webhook_cert.pem"
+KEY_OUT="webhook_pkey.pem"
+CERT_OUT="webhook_cert.pem"
 
 
 openssl req -newkey rsa:2048 -sha256 -nodes -keyout $KEY_OUT \
